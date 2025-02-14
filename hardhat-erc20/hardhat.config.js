c87103a648cba6512a31fbc18fe1154d0b708b1d
Lyubomir <lyyubo@gmail.com>
2025-02-14 16:16:18 +0200

    Updated contract SimpleToken

--- hardhat-erc20/hardhat.config.js
+++ hardhat-erc20/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("./tasks/token-tasks");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
---
+++ hardhat-erc20/tasks/token-tasks.js
@@ -0,0 +1,35 @@
+const { task } = require("hardhat/config");
+
+task("mint-tokens", "Mints tokens to a specific address")
+  .addParam("address", "The address that will receive the tokens")
+  .addParam("amount", "Amount of tokens to mint")
+  .setAction(async (taskArgs, hre) => {
+    try {
+      const SimpleToken = await hre.ethers.getContractFactory("SimpleToken");
+      const simpleToken = await SimpleToken.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3");
+
+      const amount = hre.ethers.parseEther(taskArgs.amount);
+      await simpleToken.mint(taskArgs.address, amount);
+      
+      console.log(`Minted ${taskArgs.amount} tokens to ${taskArgs.address}`);
+    } catch (error) {
+      console.error("Error minting tokens:", error);
+    }
+  });
+
+task("transfer-tokens", "Transfers tokens between addresses")
+  .addParam("to", "The address that will receive the tokens")
+  .addParam("amount", "Amount of tokens to transfer")
+  .setAction(async (taskArgs, hre) => {
+    try {
+      const SimpleToken = await hre.ethers.getContractFactory("SimpleToken");
+      const simpleToken = await SimpleToken.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3");
+
+      const amount = hre.ethers.parseEther(taskArgs.amount);
+      await simpleToken.transfer(taskArgs.to, amount);
+      
+      console.log(`Transferred ${taskArgs.amount} tokens to ${taskArgs.to}`);
+    } catch (error) {
+      console.error("Error transferring tokens:", error);
+    }
+  });
